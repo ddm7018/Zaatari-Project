33af8703147310b2475a9c392dd27ff712b72409
Daniel Mooney <ddm7018@rit.edu>
2017-03-08 13:02:59 -0500

    updating global

--- map/global.R
+++ map/global.R
@@ -4,6 +4,25 @@ library(hash)
 asset <- readRDS("cap_data/raw_assets.rds")
 #block <- readRDS("cap_data/block_summary.rds")
 
+dist <- readOGR("boundaries/template_datasets/Zaatari_reference_datasets")
+dist <- spTransform(dist, CRS("+proj=longlat +datum=WGS84"))
+
+dist$new <- 0
+
+count <- 0
+for(i in 1:nrow(data.frame(dist))){
+  #print(dist[i,])
+  count <- count + .01
+  result <- block[block$district == dist[i,]$District & block$block == dist[i,]$Block,]
+  if (nrow(result) > 0 ){
+    dist$new[i] <- count
+  }
+  else{
+    dist$new[i] <- 0
+  }
+  
+}
+
 colHash <- hash()
 colHash["total_educated"] <- "total_educated"
 colHash["total_residents"]    <- "total_residents"
